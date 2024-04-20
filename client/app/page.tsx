a3b8c9797510a8fad2c4fa069c01676ca973b951
calebvergene <calebverg@gmail.com>
2024-04-19 22:50:02 -0700

    added globe

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { GlobeDemo } from "@/components/ui/GithubGlobe";
 
 export default function Home() {
   return (
     <main className="container">
-      <div>snipe</div>
+      <GlobeDemo/>
     </main>
   );
 }
