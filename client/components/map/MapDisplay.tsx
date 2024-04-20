ac6197d53452391f3fd35f207fc309a12998ed17
Lex Ibanez <laibanez@uci.edu>
2024-04-20 16:09:59 -0700

    more circles

--- client/components/map/MapDisplay.tsx
+++ client/components/map/MapDisplay.tsx
@@ -19,9 +19,31 @@ const mapOptions = {
 
 const landmarks = {
   bruinStatue: {
-    center: { lat: 34.0720, lng: -118.4450 },
+    center: { lat: 34.0709621, lng: -118.4449902 },
+  },
+  royceHall: {
+    center: { lat: 34.0728552, lng: -118.4421766 },
+  },
+  powellLibrary: {
+    center: { lat: 34.0716399, lng: -118.4421766 },
+  },
+  invertedFountain: {
+    center: { lat: 34.07007650000001, lng: -118.4407628 },
+  },
+  pauleyPavilion: {
+    center: { lat: 34.0703423, lng: -118.4469294 },
+  },
+  sculptureGarden: {
+    center: { lat: 34.0751, lng: -118.4400333 },
+  },
+  janssSteps: {
+    center: { lat: 34.0721929, lng: -118.443168 },
+  },
+  murphyHall: {
+    center: { lat: 34.0716251, lng: -118.4387327 },
   },
 };
+
 
 function MapDisplay() {
   const mapContainerRef = useRef(null);
