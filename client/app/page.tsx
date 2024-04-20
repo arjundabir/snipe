792279e79e7422c57729723ce6d48195f4a73758
Arjun Dabir <arjunadabir@gmail.com>
2024-04-20 01:15:31 -0700

    intial commit with new local repo -arjun

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,13 +2,8 @@ import { GlobeDemo } from "@/components/ui/GithubGlobe";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <main className="container">
-      <GlobeDemo/>
-=======
-    <main className="container bg-white mx-auto">
-      <div>snipe</div>
->>>>>>> e86165c180bd6f61b61e9f7c9e7333e23548d20d
+      <GlobeDemo />
     </main>
   );
 }
