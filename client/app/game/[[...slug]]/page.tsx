3aa14731ea16a0189b2f64ef49fe55502bdaea5c
Arjun Dabir <arjunadabir@gmail.com>
2024-04-21 06:32:56 -0700

    fixed button layout on /game

--- client/app/game/[[...slug]]/page.tsx
+++ client/app/game/[[...slug]]/page.tsx
@@ -3,21 +3,17 @@ import React from "react";
 import { ScavengerHunt } from "@/components/login/ScavengerHunt";
 import { Adventure } from "@/components/login/Adventure";
 
-
 const page = () => {
   return (
     <>
-      <div className="w-screen h-screen flex flex-grid justify-center items-center">
-        <Link href={`/map`} className="mr-16">
-            <ScavengerHunt/>
+      <div className="w-dvw h-full md:h-dvh flex flex-wrap justify-center items-center gap-4 p-4">
+        <Link href={`/map`}>
+          <ScavengerHunt />
         </Link>
         <Link href={"https://journai-two.vercel.app"}>
-            <Adventure/>
-          </Link>
-      <div className='flex flex-col justify-around items-center p-4 rounded-md m-4'>
-        <div>
-          </div>
-        </div>
+          <Adventure />
+        </Link>
+        <div className="flex flex-col justify-around items-center p-4 rounded-md m-4"></div>
       </div>
     </>
   );
