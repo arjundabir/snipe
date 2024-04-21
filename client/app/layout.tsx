052730cf0a7d982ec2bbc063604c8a2e0bc46376
Lex Ibanez <laibanez@uci.edu>
2024-04-21 06:35:39 -0700

    new title name

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Snipe",
+  title: "JournAI",
   description: "Get your friends, join the fun!",
 };
 
